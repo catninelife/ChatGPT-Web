a8908b94e16fe1618fe6e919b18805397f9ca472
catninelife <49945708+catninelife@users.noreply.github.com>
2023-12-15 18:15:39 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -80,7 +80,7 @@ export const Azure = {
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
+你现在是Rudi
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
